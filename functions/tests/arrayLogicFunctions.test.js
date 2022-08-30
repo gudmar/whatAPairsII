8417fdaf51c3101cb09ec098402e232b024f88d4
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-08-31 00:25:24 +0200

    Jest installed, and function for arrays tested

---
+++ functions/tests/arrayLogicFunctions.test.js
@@ -0,0 +1,56 @@
+// import { describe } from "node:test";
+import { xor, and } from "../arrayLogicFunctions.js";
+// const { xor } = require('../arrayLogicFunctions.js');
+
+
+describe('Testing and from arrayLogicFunctions', () => {
+    it('Should return 1, 2, 3 in case these elements are common for both arrays', () => {
+        const arr1 = [0, -2, 1, 2, 3, 5, 6];
+        const arr2 = [-100, 1, 23, 2, 33, 3, 4];
+        expect (and(arr1, arr2)).toEqual([1, 2, 3])
+    })
+    it('Should return 1, 2, 3 in case these elements are common for both arrays, even if those elements repete', () => {
+        const arr1 = [0, -2, 1, 1, 2, 3, 2, 5, 3, 6];
+        const arr2 = [-100, 1, 23, 2, 33, 3, 4];
+        expect (and(arr1, arr2)).toEqual([1, 2, 3])
+    })
+    it('Should return 1, 2, 3 in case these elements are common for both arrays, even if those elements repete (change args order)', () => {
+        const arr1 = [-100, 1, 23, 2, 33, 3, 4];
+        const arr2 = [0, -2, 1, 1, 2, 3, 2, 5, 3, 6];
+        expect (and(arr1, arr2)).toEqual([1, 2, 3])
+    })
+    it ('Should return [] in case none elements are common', () => {
+        const arr1 = [1, 2, 3, 4];
+        const arr2 = [5, 6, 7, 8];
+        expect (and(arr1, arr2)).toEqual([])
+    })
+    it ('Should return [] in case first array is empty', () => {
+        const arr1 = [];
+        const arr2 = [5, 6, 7, 8];
+        expect (and(arr1, arr2)).toEqual([])
+    })
+    it ('Should return [] in case second array is empty', () => {
+        const arr1 = [1, 2, 3, 4];
+        const arr2 = [];
+        expect (and(arr1, arr2)).toEqual([])
+    })
+
+})
+
+// describe('Testing xor from arrayLogicFunctions', () => {
+//     it('Should return [] in case both arrays are equal', () => {
+//         const arr1 = [1, 3, 2, 4];
+//         const arr2 = [1, 3, 2, 4];
+//         expect(xor(arr1, arr2)).toEqual([])
+//     })
+//     it('Should return [2, 4] in case arrays are the same, but one array missed 2 and 4', () => {
+//         const arr1 = [1, 3];
+//         const arr2 = [1, 3, 2, 4];
+//         expect(xor(arr1, arr2)).toEqual([2, 4])
+//     })
+//     it('Should return [2, 4, 5] in case arrays are the same, but one array missed 2 and 4, and the other array misses 5', () => {
+//         const arr1 = [1, 3, 5];
+//         const arr2 = [1, 3, 2, 4];
+//         expect(xor(arr1, arr2)).toEqual([5, 2, 4])
+//     })
+// });
